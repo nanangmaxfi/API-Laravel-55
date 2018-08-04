d0986ec4e25855fda22aec13b39b1744c32d360f
Nanang <n.maxfi.22@gmail.com>
2018-08-04 11:16:22 +0700

    init pertama dan beberapa fungsi

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function store(Request $request){
+        return 'It works';
+    }
+
+    public function signin(Request $request){
+        return 'It works';
+    }
+}

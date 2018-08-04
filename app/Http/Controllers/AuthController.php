77275460e25212694846c4fb19e4dbed83031c60
Nanang <n.maxfi.22@gmail.com>
2018-08-04 17:51:25 +0700

    setting jwt auth dan login register token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use JWTAuth;
+use JWTAuthException;
+
 use App\User;
 
 class AuthController extends Controller
@@ -26,7 +29,27 @@ class AuthController extends Controller
             'password'=>bcrypt($password)
         ]);
 
+        $credentials = [
+            'email'=>$email,
+            'password'=>$password
+        ];
+
         if($user->save()){
+
+            $token = null;
+            try{
+                if(!$token = JWTAuth::attempt($credentials)){
+                    return response()->json([
+                        'msg' => 'Email or Password are incorrect',
+                    ], 404);
+                }
+            }
+            catch(JWTAuthException $e){
+                return response()->json([
+                    'msg' => 'failed_to_create_token',
+                ], 404);
+            }
+
             $user->signin = [
                 'href' => 'api/v1/user/signin',
                 'method' => 'POST',
@@ -34,7 +57,8 @@ class AuthController extends Controller
             ];
             $response = [
                 'msg' => 'User created',
-                'user' => $user
+                'user' => $user,
+                'token' => $token
             ];
             return response()->json($response, 201);
         }
@@ -47,6 +71,46 @@ class AuthController extends Controller
     }
 
     public function signin(Request $request){
-        return 'It works';
+        $this->validate($request,[
+            'email'=>'required|email',
+            'password'=>'required|min:5'
+        ]);
+
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        if($user = User::where('email', $email)->first()){
+            $credentials = [
+                'email'=>$email,
+                'password'=>$password
+            ];
+
+            $token = null;
+            try{
+                if(!$token = JWTAuth::attempt($credentials)){
+                    return response()->json([
+                        'msg' => 'Email or Password are incorrect',
+                    ], 404);
+                }
+            }
+            catch(JWTAuthException $e){
+                return response()->json([
+                    'msg' => 'failed_to_create_token',
+                ], 404);
+            }
+
+            $response = [
+                'msg' => 'User signin',
+                'user' => $user,
+                'token' => $token
+            ];
+            return response()->json($response, 201);
+        }
+
+        $response =[
+            'msg' => 'An error occured'
+        ];
+
+        return response()->json($response, 404);
     }
 }
---
+++ app/Http/Middleware/VerifyJWTToken.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class VerifyJWTToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        try{
+            $user = JWTAuth::toUser($request->input('token'));
+        }
+        catch(JWTException $e){
+            if($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredExeption){
+                return response()->json(['token_expired'], $e->getStatusCode());
+            }
+            else if($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidExeption){
+                return response()->json(['token_invalid'], $e->getStatusCode());
+            }
+            else{
+                return response()->json(['error'=>'Token is required']);
+            }
+        }
+        return $next($request);
+    }
+}
